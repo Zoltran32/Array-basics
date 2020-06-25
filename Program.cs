8140e5fcb598a4faaacb63ce172eb96bf2311968
Telesnitskiy Vladimir <66076094+Zoltran32@users.noreply.github.com>
2020-06-25 20:23:20 +0400

    added command input
    Added command number input and processing. Added some comments too

--- Program.cs
+++ Program.cs
@@ -12,8 +12,10 @@ namespace Array_basics
 {	
 	class Program
 	{
-		public static void userInfo()
+		public static void userInfo() 
 		{
+			//Doesn't look as good design because adding new commands can be complicated
+			//Also, there is a text typing format for console output
 			Console.WriteLine("Type command for action with array of int, that has some elements");
 			Console.WriteLine("1 - for counting summ of integers in array");
 			Console.WriteLine("2 - for finding maximum element in array");
@@ -23,23 +25,44 @@ namespace Array_basics
 	
 		public static void Main(string[] args)
 		{
-			bool canWork = true;
-			
-			Console.WriteLine("Type length for array");
+			Console.WriteLine("Type integer number for length of array of integers");
+			//Problem, user can type invalid input that will crash application
 			int size = Convert.ToInt32(Console.ReadLine());
 			int[] numbers = new int[size];
 			for(int i = 0; i < size; i++)
 			{
-				Console.WriteLine("Type {0} element of array", i+1);
+				Console.WriteLine("Type element numbers[{0}] of array", i+1);
+				//Problem, user can type invalid input that will crash application				
 				numbers[i] = Convert.ToInt32(Console.ReadLine());
 			}
 			
-			while(!canWork) 
-			{
-				//Used so many actions may be performed with array
-				//Not implemented any functionality yet				
+			int command = -1;
+			while(command != 0) 
+			{	
+				//Input filtration cycle (not implemented yet)				
+				while(command < 0 || command > 3) //Warning, magic numbers detected
+				{
+					userInfo();
+					//Problem, user can type invalid input that will crash application
+					command = Convert.ToInt32(Console.ReadLine());
+				}
+				
+				switch(command)
+				{
+					case 1:
+						//Do something
+						break;
+					case 2:
+						//Do another thing
+						break;
+					case 3:
+						//Do third thing
+						break;
+					default:
+						//Exit program. Command is 0 due to input filtration (not implemented yet)
+						break;
+				}
 			}
-			userInfo();
 			Console.WriteLine("Press any key to exit");
 			Console.ReadKey();
 		}		
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Basic array use
 Created in SharpDevelop IDE
 Idea is to create array and do something with it due to commands, typed from console
+Used also to learn how to use GitHub
