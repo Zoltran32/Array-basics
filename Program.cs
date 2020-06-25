2f013b0bb0fa7b3e6e7d2037552a659633ee1733
Telesnitskiy Vladimir <66076094+Zoltran32@users.noreply.github.com>
2020-06-25 19:04:43 +0400

    Init
    Creating SharpDevelop project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Zoltran32
+ * Date: 25.06.2020
+ * Time: 19:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Array_basics
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
